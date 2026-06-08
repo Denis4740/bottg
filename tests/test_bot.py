f2a10a54455f54441229a5bd0989ab1d3ffd0f29
Denis4740 <den.lobov.07@bk.ru>
2026-06-08 12:15:07 +0300

    Update test_bot.py

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,5 +1,5 @@
 import unittest
-from src.api_client import get_new_image
+from api_client import get_new_image
 
 class TestApiClient(unittest.TestCase):
     def test_get_new_image_returns_string_or_none(self):
